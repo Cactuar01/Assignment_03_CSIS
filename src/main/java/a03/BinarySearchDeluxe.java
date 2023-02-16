2ee925ea334f1bca9406a329f93924ea962d2e3b
Cactuar01 <Cactuar001@gmail.com>
2023-02-16 15:06:56 -0700

    binary search might be done

--- src/main/java/a03/BinarySearchDeluxe.java
+++ src/main/java/a03/BinarySearchDeluxe.java
@@ -3,14 +3,87 @@ package a03;
 import java.util.Comparator;
 
 public class BinarySearchDeluxe {
-    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator){
+
+    // Return the index of the first key in a[] that equals the search key, or -1 if no such key.
+    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
         if(a == null || key == null || comparator == null){
             throw new NullPointerException();
         }
-        return 0;
+        int low = 0;
+        int high = a.length -1;
+        int mid;
+        if (comparator.compare(a[low], key) == 0) {
+            return low;
+        }
+        while(low <= high){
+            mid = low + (high - low) / 2;
+            if(comparator.compare(key, a[mid]) < 0){
+                high = mid - 1;
+            }else if(comparator.compare(key, a[mid]) > 0){
+                low = mid + 1;
+            }else if(mid < a.length - 1 && comparator.compare(key, a[mid - 1]) == 0) {
+                high = mid - 1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
     }
-    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator){
 
-        return 0;
+    // Return the index of the last key in a[] that equals the search key, or -1 if no such key.
+    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator){
+        if(a == null || key == null || comparator == null){
+            throw new NullPointerException();
+        }
+        int low = 0;
+        int high = a.length -1;
+        int mid;
+        if (comparator.compare(a[high], key) == 0) {
+            return high;
+        }
+        while(low <= high){
+            mid = low + (high - low) / 2;
+            if(comparator.compare(key, a[mid]) < 0){
+                high = mid - 1;
+            }else if(comparator.compare(key, a[mid]) > 0){
+                low = mid + 1;
+            }else if(mid < a.length - 1 && comparator.compare(key, a[mid + 1]) == 0) {
+                low = mid + 1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
     }
+
+    public static void main(String[] args) {
+        Integer[] arr = {1, 2, 3, 4, 5};
+
+        int lastIndex = lastIndexOf(arr, 3, Comparator.naturalOrder());
+        System.out.println("Last index of 3 in {1, 2, 3, 4, 5}: " + lastIndex);
+
+        lastIndex = lastIndexOf(arr, 6, Comparator.naturalOrder());
+        System.out.println("Last index of 6 in {1, 2, 3, 4, 5}: " + lastIndex);
+
+        lastIndex = lastIndexOf(arr, 1, Comparator.naturalOrder());
+        System.out.println("Last index of 1 in {1, 2, 3, 4, 5}: " + lastIndex);
+
+        lastIndex = lastIndexOf(arr, 5, Comparator.naturalOrder());
+        System.out.println("Last index of 5 in {1, 2, 3, 4, 5}: " + lastIndex);
+
+        System.out.println();
+
+        int firstIndexOf = firstIndexOf(arr, 3, Comparator.naturalOrder());
+        System.out.println("First index of 3 in {1, 2, 3, 4, 5}: " + firstIndexOf);
+
+        firstIndexOf = firstIndexOf(arr, 6, Comparator.naturalOrder());
+        System.out.println("First index of 6 in {1, 2, 3, 4, 5}: " + firstIndexOf);
+
+        firstIndexOf = firstIndexOf(arr, 1, Comparator.naturalOrder());
+        System.out.println("First index of 1 in {1, 2, 3, 4, 5}: " + firstIndexOf);
+
+        firstIndexOf = firstIndexOf(arr, 5, Comparator.naturalOrder());
+        System.out.println("First index of 5 in {1, 2, 3, 4, 5}: " + firstIndexOf);
+    }
+
 }
--- src/main/java/a03/Term.java
+++ src/main/java/a03/Term.java
@@ -2,7 +2,9 @@ package a03;
 
 import java.util.Comparator;
 
-public class Term implements Comparable<Term>{
+public class Term implements Comparable<Term> {
+
+    // Initialize a term with the given query string and weight.
     String query;
     double weight;
     public Term(String query, double weight){
@@ -14,19 +16,24 @@ public class Term implements Comparable<Term>{
         this.query = query;
         this.weight = weight;
     }
+
+    // Compare the terms in descending order by weight.
     public static Comparator<Term> byReverseWeightOrder(){
         return null;
     }
+
+    // Compare the terms in lexicographic order but using only the first r characters of each query.
     public static Comparator<Term> byPrefixOrder(int r){
-        if(r < 0){
-            throw new IllegalArgumentException();
-        }
         return null;
     }
-    @Override
-    public int compareTo(Term o) {
-        return this.query.compareToIgnoreCase(o.query);
+
+    // Compare the terms in lexicographic order by query.
+    public int compareTo(Term that){
+        return this.query.compareTo(that.query);
     }
+
+    // Return a string representation of the term in the following format:
+    // the weight, followed by a tab, followed by the query.
     public String toString(){
         return null;
     }
